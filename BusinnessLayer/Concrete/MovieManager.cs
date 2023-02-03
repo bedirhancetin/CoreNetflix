deefa094f7588b5fbb43e2466d18bfcb728abfd5
bedirhancetin <116276593+bedirhancetin@users.noreply.github.com>
2023-02-03 14:41:27 +0300

    Add files via upload

---
+++ BusinnessLayer/Concrete/MovieManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinnessLayer.Abstract;
+using DataAceesLayer.Abstract;
+using DataAceesLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinnessLayer.Concrete
+{
+	public class MovieManager : IMovieService
+	{
+		IMovieDal _movieDal;
+
+		public MovieManager(IMovieDal movieDal)
+		{
+			_movieDal = movieDal;
+		}
+
+		public void Add(Movie t)
+		{
+			_movieDal.Insert(t);		}
+
+		public List<Movie> GetAllList()
+		{
+			return _movieDal.GetListAll();
+		}
+
+		public Movie GetById(int id)
+		{
+			return _movieDal.GetByID(id);		}
+
+		public void Remove(Movie t)
+		{
+			_movieDal.Delete(t);
+
+
+		}
+			public void Update(Movie t)
+		{
+			_movieDal.Update(t);		}
+	}
+}
